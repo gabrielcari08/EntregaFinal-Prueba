944f119400d9dda57f8b401937e33efd3f9e1ac8
gabrielcari08 <gabrielcari2008@gmail.com>
2025-04-28 22:02:57 -0300

    logout aplicado

--- futbolblog/accounts/urls.py
+++ futbolblog/accounts/urls.py
@@ -4,7 +4,7 @@ from .views import UserLoginView, UserLogoutView, UserRegisterView, ProfileView,
 urlpatterns = [
     path('', WelcomeView.as_view(), name='welcome'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('accounts/logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('accounts/profile/', ProfileView.as_view(), name='profile'),
     path('accounts/edit_profile/', EditProfileView.as_view(), name='edit_profile'),
