89d3738afdf0a4a7aaaa17a2082304ed50584ebe
gabrielcari08 <gabrielcari2008@gmail.com>
2025-05-01 11:52:50 -0300

    Aplicacion de estilos css finalizada

--- futbolblog/futbolblog/settings.py
+++ futbolblog/futbolblog/settings.py
@@ -119,7 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Asegura que Django busque en esta carpeta
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
